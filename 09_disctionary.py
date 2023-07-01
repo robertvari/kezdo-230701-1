2a46283783242fcaace5a24cf118212ae68e2571
Robert Vari <mail.robertvari@gmail.com>
2023-07-01 16:54:44 +0200

    update

---
+++ 09_disctionary.py
@@ -0,0 +1,8 @@
+phonebook = {
+    "06201234567": {"name": "Kiss Csaba", "email": "csaba@gmail.com"},
+    "06209876543": {"name": "Nagy Krisztina", "email": "kriszta@gmail.com"},
+    "06205448535": {"name": "Kovács Géza", "email": "geza@gmail.com"},
+    "06309871234": {"name": "Nagy Csaba", "email": "ncsaba@gmail.com"},
+}
+
+print(phonebook["test_data"])
