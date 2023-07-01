d344ada4b0f0a6b87781ad450621eb4cb9bafa71
Robert Vari <mail.robertvari@gmail.com>
2023-07-01 15:12:27 +0200

    update

---
+++ 03_variables.py
@@ -0,0 +1,7 @@
+name = "Csaba"
+address = "Pécs"
+email = "csaba@gmail.com"
+age = 34
+
+# f"..." = formated string
+print(f"Szia {name}. Te {address}en élsz. Az emailcímed pedig {email} és {age} éves vagy.")
