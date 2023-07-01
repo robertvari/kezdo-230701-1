15c2bf35dfda41a8f0a841a77f301eb9977210f7
Robert Vari <mail.robertvari@gmail.com>
2023-07-01 16:33:52 +0200

    update

---
+++ 08_sets.py
@@ -0,0 +1,22 @@
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+
+# union
+#print(a | b)
+
+# difference
+#print(a-b)
+#print(b-a)
+
+# intersection
+#print(a & b)
+
+# symmetrical difference
+#print(a ^ b)
+
+print(b)
+b.add(7)
+print(b)
+
+b.remove(3)
+print(b)
