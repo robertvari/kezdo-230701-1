97f0021cfbe80ba5f475d369f2708ba79331fed9
Robert Vari <mail.robertvari@gmail.com>
2023-07-02 15:01:25 +0200

    update

---
+++ magicnumber.py
@@ -0,0 +1,26 @@
+import platform, os, random
+
+# Create game variables
+min = 1
+max = 10
+max_tries = 3
+magic_number = random.randint(min, max)
+magic_number = 5  # TODO REMOVE THIS
+
+# Clear terminal
+if platform.system() == "Windows":
+    os.system("cls")
+else:
+    os.system("clear")
+
+# Print game intro
+print("-"*50, "MAGIC NUMBER GAME", "-"*50)
+print(f"I have a number between {min} and {max}. Can you guess it?")
+print(f"You can try {max_tries} times.")
+
+player_guess = input("What is your guess?")
+
+if str(magic_number) == player_guess:
+    print("You win!")
+else:
+    print(f"You lost. My number was {magic_number}")
