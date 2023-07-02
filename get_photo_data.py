4c21dff378b1243bee437d7fb6bc44408a8b5427
Robert Vari <mail.robertvari@gmail.com>
2023-07-02 16:01:20 +0200

    update

---
+++ get_photo_data.py
@@ -0,0 +1,15 @@
+import os
+
+# r = raw string
+folder_path = r"D:\Work\_PythonSuli\kezdo-230701\photos"
+
+# check if path exists
+assert os.path.exists(folder_path), f"Path does not exist: {folder_path}"
+
+# check if folder_path is a directory
+assert os.path.isdir(folder_path), f"Path must be a directory. {folder_path}"
+
+# get all files and folders from folder_path
+files = os.listdir(folder_path)
+
+print(files)
