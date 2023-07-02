e6dc8ee7c158e54727254137746d298e25118769
Robert Vari <mail.robertvari@gmail.com>
2023-07-02 16:42:04 +0200

    update

--- get_photo_data.py
+++ get_photo_data.py
@@ -11,7 +11,15 @@ assert os.path.isdir(folder_path), f"Path must be a directory. {folder_path}"
 
 # get all files and folders from folder_path
 files = os.listdir(folder_path)
+allowed_extensions = [".jpg", ".jpeg"]
 
 for i in files:
     full_path = os.path.join(folder_path, i)
-    print(full_path)
+    
+    # check if extension in allowed_extensions
+    extension = os.path.splitext(full_path)[-1]
+    if not extension.lower() in allowed_extensions:
+        continue
+
+    # open image
+    
