19408fd7119860fd2e9f9615b75fde5abe68a3af
Robert Vari <mail.robertvari@gmail.com>
2023-07-02 15:27:39 +0200

    update

--- magicnumber.py
+++ magicnumber.py
@@ -3,8 +3,6 @@ import platform, os, random
 # Create game variables
 min = 1
 max = 10
-max_tries = 3
-magic_number = random.randint(min, max)
 
 # Clear terminal
 if platform.system() == "Windows":
@@ -15,23 +13,32 @@ else:
 # Print game intro
 print("-"*50, "MAGIC NUMBER GAME", "-"*50)
 print(f"I have a number between {min} and {max}. Can you guess it?")
-print(f"You can try {max_tries} times.")
-
-player_guess = input("What is your guess?")
 
 
-while str(magic_number) != player_guess:
-    max_tries -= 1
-    if max_tries == 0:
-        break
 
-    print(f"Wrong guess. You can try {max_tries} more times.")
+# Game Loop
+while True:
+    max_tries = 3
+    magic_number = random.randint(min, max)
+    print(f"You can try {max_tries} times.")
+
     player_guess = input("What is your guess?")
+    while str(magic_number) != player_guess:
+        max_tries -= 1
+        if max_tries == 0:
+            break
 
+        print(f"Wrong guess. You can try {max_tries} more times.")
+        player_guess = input("What is your guess?")
 
+    # Check final result
+    if str(magic_number) == player_guess:
+        print("You win!")
+    else:
+        print(f"You lost. My number was {magic_number}")
 
-# Check final result
-if str(magic_number) == player_guess:
-    print("You win!")
-else:
-    print(f"You lost. My number was {magic_number}")
+    # Ask player if he/she wants to play again.
+    player_choice = input("Do you want to play again? (y/n)")
+    if player_choice.lower() != "y":
+        print("Ok... maybe next time... ")
+        exit()
